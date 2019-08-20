77e016521ee8c52a2cfc270f89dbb0656160e70e
EmirVatric <vatricemir@gmail.com>
2019-08-20 21:12:19 +0200

    class made

--- main.rb
+++ main.rb
@@ -2,15 +2,45 @@ require 'rest-client'
 require 'rubygems'
 require 'nokogiri'
 
-puts "Welcome "
-puts "Plea"
 
-link = "https://www.bing.com/search"
-html= RestClient.get link, {params: {q: "ruby"}}
 
-ng = Nokogiri::HTML(html)
-titles=[]
-ng.css('#b_results>li h2,#b_results>li p,#b_results>li cite').each do |x|
-  titles << x.text
+class Bing
+  
+  def query (input)
+    link = "https://www.bing.com/search"
+    html= RestClient.get link, {params: {q: "#{input}"}}
+    ng = Nokogiri::HTML(html)
+    titles=[]
+    counter = 0
+    ng.css('#b_results>li h2,#b_results>li p,#b_results>li cite').each do |x|
+      if counter % 3 == 0
+        titles << "\n"
+      end
+      titles << x.text
+      counter += 1
+    end
+    response(titles)
+  end
+
+  def response(response)
+    puts response
+  end
+
 end
-puts titles
+
+action = true
+
+while action
+  search = Bing.new
+  puts 'Please enter the search term'
+  input = gets.chomp
+  search.query(input)
+
+  puts 'Do You want to search again? Y/N'
+  answer = gets.chomp.capitalize
+  if answer != 'Y'
+    action = false
+  end
+
+end
+
